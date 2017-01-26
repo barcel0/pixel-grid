904204072408710a42b4bbb35563a75b36469c14
barcel0 <lbarcelocarrera@gmail.com>
2017-01-26 20:56:30 +0000

    reset button added

--- js/pixelGrid.js
+++ js/pixelGrid.js
@@ -1,21 +1,29 @@
 $(document).ready(function(){
-   var gridSize = prompt('Choose grid size');
-   var gridTotal = gridSize*gridSize;
    
-   for (i = 0; i < gridSize; ++i) {
-      $('.grid').append('<div class="gridRow"></div>');
-   }
-      for (i = 0; i < gridSize; ++i) {
-      $('.gridRow').append('<div class="pixel"></div>');
-   }
-   
-   
-  
-   $('.pixel').mouseenter(function(){
-      var bgColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-      $(this).css('background-color', bgColor);
-      //$(this).addClass('pixelHovered');
+   function initGrid(){
+         $('.grid').empty('div');
+         var gridSize = prompt('Choose grid size');
+ 
+         for (i = 0; i < gridSize; ++i) {
+            $('.grid').append('<div class="gridRow"></div>');
+         }
+            for (i = 0; i < gridSize; ++i) {
+            $('.gridRow').append('<div class="pixel"></div>');
+         }
+
+          
+         $('.pixel').mouseenter(function(){
+            var bgColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+            $(this).css('background-color', bgColor);
+
+         });
+         
+   };
+
+   initGrid();
+
+   $('.resetButton').click(function(){
+      initGrid();
    });
-   
 
 });
